903b150c320d96a8b94f55ce5827ddc6ad62b0fe
Nicolas <end3rbyte@outlook.com>
2026-08-04 21:20:16 +0200

    Replace build aggregate with LandoriaGamePack

--- LandoriaGamePack/Properties/AssemblyInfo.cs
+++ LandoriaGamePack/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Landoria.Build")]
+[assembly: AssemblyTitle("LandoriaGamePack")]
 [assembly: AssemblyDescription("Landoria development aggregate build project")]
 [assembly: AssemblyCompany("Landoria")]
-[assembly: AssemblyProduct("Landoria.Build")]
+[assembly: AssemblyProduct("LandoriaGamePack")]
 [assembly: ComVisible(false)]
 [assembly: Guid("88745306-66d4-48b4-99a1-856f835f0707")]
 [assembly: AssemblyVersion("1.0.0.*")]
---
+++ LandoriaGamePack/README.md
@@ -0,0 +1,9 @@
+# LandoriaGamePack
+
+`LandoriaGamePack` is the development-only aggregate project. Building it compiles every standalone Landoria mod referenced by the solution.
+
+It is not a BepInEx plugin, is never installed in Valheim, and is not published as a mod.
+
+## Contact
+
+Report bugs through [GitHub Issues](https://github.com/end3rbyte/LandoriaMods/issues). For other conversations, use [GitHub Discussions](https://github.com/end3rbyte/LandoriaMods/discussions).
