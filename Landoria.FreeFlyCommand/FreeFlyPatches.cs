15aedf72e760a1bea3b2031976b6475cf02ff695
Nicolas PUZIN <end3rbyte@outlook.com>
2026-08-10 17:10:34 +0200

    Preserve_FreeFlyCommand_smoothing (#27)

--- Landoria.FreeFlyCommand/FreeFlyPatches.cs
+++ Landoria.FreeFlyCommand/FreeFlyPatches.cs
@@ -38,7 +38,6 @@ namespace Landoria.FreeFlyCommand
     {
         private static void Postfix(GameCamera __instance)
         {
-            FreeFlyController.EnableSmoothness();
             FreeFlyController.ClampToPlayer(__instance);
         }
     }
