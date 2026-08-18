86651d2e4ae070e78e6ec643093ac4b61a0f0dc0
end3rbyte <end3rbyte@outlook.com>
2026-08-18 22:23:40 +0200

    Mark clients with verified modpack inventories (#299)

---
+++ Landoria.SharedLib/VerifiedModpackMarker.cs
@@ -0,0 +1,38 @@
+using System.Linq;
+
+namespace Landoria.SharedLib
+{
+    public static class VerifiedModpackMarker
+    {
+        private const string Key = "landoria.verified_modpack";
+        private const string Value = "1";
+
+        public static void Mark(ZRpc rpc)
+        {
+            ZNetPeer peer = FindPeer(rpc);
+            if (peer != null)
+            {
+                peer.m_serverSyncedPlayerData[Key] = Value;
+            }
+        }
+
+        public static bool IsMarked(string hostName)
+        {
+            return ZNet.instance?.GetPeers().Any(peer =>
+                peer?.m_socket?.GetHostName() == hostName &&
+                peer.m_serverSyncedPlayerData.TryGetValue(Key, out string value) &&
+                value == Value) == true;
+        }
+
+        public static void Unmark(ZRpc rpc)
+        {
+            FindPeer(rpc)?.m_serverSyncedPlayerData.Remove(Key);
+        }
+
+        private static ZNetPeer FindPeer(ZRpc rpc)
+        {
+            return ZNet.instance?.GetPeers()
+                .FirstOrDefault(peer => ReferenceEquals(peer.m_rpc, rpc));
+        }
+    }
+}
