345774344578ca857e64fca44f7a393549e55bc1
landoria-automation <landoria-automation@users.noreply.github.com>
2026-08-21 21:43:54 +0000

    Create LandoriaModPack 1.0.27-draft

--- Landoria.LandoriaModPack/Properties/AssemblyInfo.cs
+++ Landoria.LandoriaModPack/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("BD9DB129-FFA6-414E-95AD-10EA8CE362C4")]
-[assembly: AssemblyVersion("1.0.26.*")]
-[assembly: AssemblyFileVersion("1.0.26")]
+[assembly: AssemblyVersion("1.0.27.*")]
+[assembly: AssemblyFileVersion("1.0.27")]
 
-[assembly: AssemblyInformationalVersion("1.0.26")]
+[assembly: AssemblyInformationalVersion("1.0.27-draft")]
